1e79402bc52e6dfae125e4678e5a751a7c58f002
Jhon Bocanegra <jhon.bocanegra26@gmail.com>
2021-10-16 22:46:24 -0500

    Agregado menu interactivo con inquirer

---
+++ helpers/inquirer.js
@@ -0,0 +1,68 @@
+const inquirer = require('inquirer')
+require('colors')
+
+const questions = [
+  {
+    type: 'list',
+    name: 'opcion',
+    message: '¿Que desea hacer?',
+    choices: [
+      {
+        value: '1',
+        name: '1. Crear tarea',
+      },
+      {
+        value: '2',
+        name: '2. Listar tarea',
+      },
+      {
+        value: '3',
+        name: '3. Listar tareas completadas',
+      },
+      {
+        value: '4',
+        name: '4. Listar tareas pendientes',
+      },
+      {
+        value: '5',
+        name: '5. Completar tarea(s)',
+      },
+      {
+        value: '6',
+        name: '6. Borrar tarea',
+      },
+      {
+        value: '0',
+        name: '0. Salir',
+      },
+    ],
+  },
+]
+
+const inquirerMenu = async () => {
+  console.clear()
+  console.log('======================='.blue)
+  console.log(' Selecione una opción '.blue)
+  console.log('=======================\n'.blue)
+
+  const { opcion } = await inquirer.prompt(questions)
+
+  return opcion
+}
+
+const pause = async () => {
+  const question = [
+    {
+      type: 'input',
+      name: 'enter',
+      message: `Presione ${'ENTER'.blue} para continuar`,
+    },
+  ]
+  console.log('\n')
+  await inquirer.prompt(question)
+}
+
+module.exports = {
+  inquirerMenu,
+  pause,
+}
--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 require('colors')
+// const { showMenu, pause } = require('./helpers/messages')
+const { inquirerMenu, pause } = require('./helpers/inquirer')
 console.clear()
 
-const main = () => {
+const main = async () => {
   console.log('Inicio de la aplicacion')
+
+  let opt = ''
+
+  do {
+    opt = await inquirerMenu()
+    console.log({ opt })
+
+    await pause()
+
+    // if (opt !== '0') await pause()
+  } while (opt !== '0')
 }
 
 main()
